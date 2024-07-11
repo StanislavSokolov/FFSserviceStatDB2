88e60b14f377b6cdf9db17562efc9547ad27fed2
sokolovstanislav <ignateva-1946@mail.ru>
2024-07-11 18:49:38 +0300

    New reading properties.

--- src/main/java/org/example/Update.java
+++ src/main/java/org/example/Update.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
 import java.util.List;
 
 import static java.lang.Integer.parseInt;
@@ -28,9 +29,22 @@ public class Update extends Thread {
 
     private void update() {
 
-        SessionFactory sessionFactory = new Configuration().
-                addAnnotatedClass(Item.class).
-                addAnnotatedClass(Year.class).buildSessionFactory();
+        SessionFactory sessionFactory = null;
+        try {
+            sessionFactory = new Configuration().
+                    addAnnotatedClass(Item.class).
+                    addAnnotatedClass(Year.class).
+                    setProperty("hibernate.driver_class", Settings.getProperties("hibernate.driver_class")).
+                    setProperty("hibernate.connection.url", Settings.getProperties("hibernate.connection.url")).
+                    setProperty("hibernate.connection.username", Settings.getProperties("hibernate.connection.username")).
+                    setProperty("hibernate.connection.password", Settings.getProperties("hibernate.connection.password")).
+                    setProperty("hibernate.dialect", Settings.getProperties("hibernate.dialect")).
+                    setProperty("hibernate.current_session_context_class", Settings.getProperties("hibernate.current_session_context_class")).
+                    setProperty("hibernate.show_sql", Settings.getProperties("hibernate.show_sql")).
+                    buildSessionFactory();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Session session = sessionFactory.getCurrentSession();
 
         try {
@@ -41,7 +55,7 @@ public class Update extends Thread {
                 List<Item> itemsSales = session.createQuery("FROM Item where sdate like '" + Data.getData(i) + "' and status like 'sold'").getResultList();
                 List<Item> itemsReturns = session.createQuery("FROM Item where sdate like '" + Data.getData(i) + "' and status like 'returned'").getResultList();
                 if (!itemsOrders.isEmpty() || !itemsSales.isEmpty()) {
-                    System.out.println("Hello");
+//                    System.out.println("Hello");
                     List<Year> day = session.createQuery("FROM Year where cdate LIKE '" + Data.getData(i) + "'").getResultList();
                     if (day.isEmpty()) {
                         Year year = new Year(Data.getData(i), itemsOrders.size(), itemsSales.size(), 0);
